f6ab20d4bd36bd4969196c9373d18da5d41cded4
Konstantin Kolyagin <37573456+kkolyagin@users.noreply.github.com>
2022-10-31 20:30:58 +0300

    3b

--- scala/app.scala
+++ scala/app.scala
@@ -1,5 +1,6 @@
 object app {
   def main(args: Array[String]): Unit = {
+    //3a
     var Str="Hello, Scala!"
     //выводит фразу «Hello, Scala!» справа налево
     println(Str.foldLeft(List[Char]()){(x,y)=>y::x}.mkString(""))
@@ -10,14 +11,18 @@ object app {
     //добавляет в конец фразы «and goodbye python!»
     println(Str.replace("!","")+ " and goodbye python!")
 
-    println("Введите:\nзначение годового дохода:")
-    val sumTotal = scala.io.StdIn.readInt()
-    println("размер премии в процентах от годового дохода:")
-    val sumAward = scala.io.StdIn.readInt()
-    println("компенсация питания:")
-    val sumFoodAllowance = scala.io.StdIn.readInt()
+    //3b
+    val sumTotal: Double = 880
+    println("Значение годового дохода: " + sumTotal)
+    val sumAward: Double = 56 //% это госслужба до 2023, далее оклады увеличат в 3 раза процент снизят, сумма не изменится.
+    println("размер премии в процентах от годового дохода: " + sumAward + " %")
+    val sumFoodAllowance: Double = 26.4 //все постоянные ежемесячные доплаты
+    println("компенсация питания: " + sumFoodAllowance)
+    val oklad = (sumTotal * (1 - sumAward / 100) / 12 - sumFoodAllowance) * 0.87
+    printf("Оклад за вычетом ПДН = %.2f", oklad)
 
-
+    //3c
+    
 
 
   }
